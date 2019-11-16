e852696a7246dd5bd702f4a290cca12eb7e232ab
Marcos Alexandre <marcos_alexr@hotmail.com>
2019-11-15 23:10:37 -0300

    Ajustes na hora da notificaçao

--- src/controllers/notificacao.js
+++ src/controllers/notificacao.js
@@ -7,7 +7,7 @@ const database = require("../models/database")()
 exports.busca_notificacoes = (req, res) => {
 	let id_usuario = req.params.id_usuario
 
-	database.query("SELECT * FROM tb_notificacao WHERE id_usuario = ?", id_usuario, (err, rows, fields) => {
+	database.query("SELECT id_usuario, id_ideia, id_evento, msg_notificacao, link_notificacao, tp_notificacao, visualizada, DATE_ADD(momento_notificacao, INTERVAL - 2 HOUR) momento_notificacao, id_usuario_acao FROM tb_notificacao WHERE id_usuario = ?", id_usuario, (err, rows, fields) => {
 		if(err){
 			return res.status(403).send({err: err}).end()
 		}else{
